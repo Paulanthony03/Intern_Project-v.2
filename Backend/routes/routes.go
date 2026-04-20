6a20e109894c6ef076840cd91bb69e9d8fbd085b
INTERN6-PMANGUIAT <intern6-pmanguiat@INTERN6-PMANGUIAT.local>
2026-04-20 11:35:04 +0800

    Last Backend Update

--- Backend/routes/routes.go
+++ Backend/routes/routes.go
@@ -20,6 +20,6 @@ func SetupRoutes(r *gin.Engine) {
 	}
 	r.POST("/api/forgot-password", handlers.ForgotPassword)
 	r.POST("/api/reset-password", handlers.ResetPassword)
-	r.DELETE("/users/:id", handlers.DeleteUser)
+	r.DELETE("/api/users/:id", handlers.DeleteUser)
 
 }
